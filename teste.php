28a2e80bb556e6c9e71377a0a66370adc7e42455
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2023-01-08 10:49:00 -0300

    Adição de arquivo para teste

--- teste.php
+++ teste.php
@@ -1,33 +1,43 @@
-<?php 
+<?php
 
-include 'teste.html';
-include './PHPMailer/lib/vendor/autoload.php';
 
-class forpen{
-    
-    
-    protected function parserString($array, $string)
+
+require_once './lib/vendor/autoload.php';
+
+class teste{
+
+    public function parserString($array, $string)
     {
-
         foreach ($array as $key => $value) {
-            $string = str_replace('[$key]', $value, $string);
+            $string = str_replace( $value,  '[ . $key . ]', $string);
         }
         return $string;
     }
 }
 
-$array = array(
-    "nome" => "Yago",
-    "nomeSobrenome" => "Yago Barbosa",
-    "mensagem" => "Ola mundo"
-);
+
+
+$arra =[
+    '[nome]' => 'Yago',
+    '[nome_sobrenome]' => 'Yago Barbosa',
+    '[mensagem]' => 'esta mensagem é um teste'
+    
+];
+
+#$mensagem = new teste();
 
 $email = fopen('./template.html', 'r+');
+
 $conteudo = fread($email, filesize('./template.html'));
-$nome = str_replace('[nome]', $array["nome"], $conteudo);
-$nomeSobre = str_replace('[nome_sobrenome]', $array["nomeSobrenome"], $nome);
-$mensagem = str_replace('[mensagem]', $array["mensagem"], $nomeSobre);
 
 
-echo $mensagem;
+foreach ($arra as $key => $value) {
+
+    $conteudo = str_replace([$key], $value , $conteudo);
+    
+}
+
+var_dump($conteudo) ;
+
+
 fclose($email);
